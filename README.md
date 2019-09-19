39e88f573829fa88652bd8a952a8e7ab7898b8a7
Vladimir Pomsztein <vpomsztein@dc.uba.ar>
2019-09-19 20:53:12 -0300

    Feature: Niveles

--- README.md
+++ README.md
@@ -13,25 +13,25 @@ Luego, se sugiere agregar unas líneas al bashrc. Hacer eso, **REINICIAR LA CONS
 ```
 pyenv install 3.6.5
 pyenv global 3.6.5
-pyenv virtualenv 3.6.5 tp3
+pyenv virtualenv 3.6.5 tp2
 ```
 
 En el directorio del proyecto
 
 ```
-pyenv activate tp3
+pyenv activate tp2
 ```
 
 ### Directamente con python3
 ```
-python3 -m venv tp3
+python3 -m venv tp2
 source tp3/bin/activate
 ```
 
 ### Con Conda
 ```
-conda create --name tp3 python=3.6.5
-conda activate tp3
+conda create --name tp2 python=3.6.5
+conda activate tp2
 ```
 
 ## Instalación de las depencias
@@ -52,11 +52,20 @@ jupyter notebook
 
 
 ## Compilación
-Ejecutar la primera celda del notebook `TP3.ipynb` o seguir los siguientes pasos o compilar el código C y asm a mano y pasar el ejecutable tp2 de src/build a exp.
+Se sugiere tener un notebook distinto por filtro para evitar problemas de mergeo con git.
+
+Ejecutar la primera celda del notebook correspondiente al filtro que se quiere testear `.ipynb` o seguir los siguientes pasos o compilar el código C y asm a mano y copiar el ejecutable tp2 de src/build a exp.
 
 ## Correr el código
 Dentro de cada celda podemos hacer nuestros tests en python, y para correr un filtro debemos hacerlo de la siguiente manera:
 
 ```
-
+call([\"./tp2\", \"args\", \"to\", \"spa\"])
 ```
+
+Por ejemplo:
+```
+call([\"./tp2\", \"Nivel\", \"-i\", \"asm\", \"img/Puente.bmp\", \"7\"])
+```
+
+Hay más información dentro del notebook, sugiero correr las primeras celdas y leer los outputs.
--- src/filters/Nivel_asm.asm
+++ src/filters/Nivel_asm.asm
@@ -1,3 +1,63 @@
+; La estructura de pixel es:
+; *----+-------------------+
+; |    |                   |
+; | 8b | char b            |
+; |    |                   |
+; +------------------------+
+; |    |                   |
+; | 8b | char g            |
+; |    |                   |
+; +------------------------+
+; |    |                   |
+; | 8b | char r            |
+; |    |                   |
+; +------------------------+
+; |    |                   |
+; | 8b | char a            |
+; |    |                   |
+; +----+-------------------+
+
+section .text
 global Nivel_asm
-Nivel_asm:
-ret
+Nivel_asm:  ; RDI = pixel[][] src
+            ; RSI = pixel[][] dst
+            ; EDX = int width
+            ; ECX = int height
+            ; R8D = int src_row_size
+            ; R9D = int dst_row_size
+            ; [rsp + 8] = int n_index
+    xor r10, r10
+    mov r10d, ecx               ; R10D = height
+
+    xor rcx, rcx                ; Limpio RCX
+    mov ecx, dword[rsp + 8]     ; ECX = n_index
+
+    mov r11, 1                  ; Preparo máscara
+    shl r11, cl                 ; Shifteo n_index bits
+    movq xmm1, r11              ; Meto la máscara en xmm1
+    pxor xmm2, xmm2             ; Creo máscara para pshufb en xmm2
+    pshufb xmm1, xmm2           ; Broadcasteo el primer byte en todos
+
+    mov rax, r10                ; RAX = height (64 bits)
+    mul rdx                     ; RAX = width * height
+    mov rcx, rax                ; RCX = Cantidad total de píxeles
+    shr rcx, 2                  ; Voy a levantar de a 4 píxeles, así que divido por 4
+.loop:
+    lea r11, [rcx * 2]                  ; R11 = RCX *2 (así rcx * 2 * 8 = rcx * 16)
+    movdqu xmm0, [rdi + r11 * 8 - 16]   ; Levanto 4 píxeles de src (16 bytes)
+    pand xmm0, xmm1                     ; Hago AND con la máscara
+    pcmpeqb xmm0, xmm2                  ; Por cada byte, si es igual a 0...
+                                        ; Entonces debo escribir 0 en ese byte. Sino, 0xFF (255)
+                                        ; Para ello, necesito negar el resultado del cmp
+    pcmpeqb xmm3, xmm3                  ; xmm3 = 0xFFFFFFFF...
+    pxor xmm0, xmm3                     ; xmm0 = !xmm0, acá tengo los bytes que quiero asignar!
+    mov eax, 0xFF000000
+    movq xmm3, rax
+    pshufd xmm3, xmm3, 0x00             ; xmm3 = 0x000000FF000000FF000000FF000000FF
+    por xmm0, xmm3                      ; Alphas tienen que ser siempre 255
+    
+    movdqu [rsi + r11 * 8 - 16], xmm0   ; Almaceno los 4 píxeles en dst
+
+    loop .loop
+.end:
+    ret                                 ; No' fuimo'
