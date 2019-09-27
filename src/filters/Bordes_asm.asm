45e20fd5905a7813fdffac7b1a033d715fb2ccc7
Ignacio Alonso Rehor <ialonso@ws22.labo2>
2019-09-26 22:01:38 -0300

    gdb debugging

--- src/filters/Bordes_asm.asm
+++ src/filters/Bordes_asm.asm
@@ -29,9 +29,6 @@ Bordes_asm:
     sub rax, r8
     ;sub rax, 1
     
-    pxor xmm0, xmm0
-    pxor xmm15, xmm15
-   
     lea r11, [rdi + r8 +1]
     lea r10, [r11]
     sub r10, r8
@@ -42,32 +39,46 @@ Bordes_asm:
     .loop:
         cmp r11, r14
         jge .end
+
+        
+        pxor xmm15, xmm15
+
 ;       ↖ ↑ ↗  =  xmm1 xmm2 xmm3
 ;       ← ⋅ →  =  xmm4   0   xmm6
 ;       ↙ ↓ ↘  =  xmm7 xmm8 xmm9
-        movdqu xmm1, [r10 - 1]  ; xmm1 = ↖
-        movdqu xmm2, [r10]      ; xmm2 = ↑
-        movdqu xmm3, [r10 + 1]  ; xmm3 = ↗
-        movdqu xmm4, [r11 - 1]  ; xmm4 = ←
-        movdqu xmm6, [r11 + 1]  ; xmm6 = →
-        movdqu xmm7, [r12 - 1]  ; xmm7 = ↙
-        movdqu xmm8, [r12]      ; xmm8 = ↓
-        movdqu xmm9, [r12 + 1]  ; xmm9 = ↘
+        movq xmm1, [r10 - 1]  ; xmm1 = ↖
+        movq xmm2, [r10]      ; xmm2 = ↑
+        movq xmm3, [r10 + 1]  ; xmm3 = ↗
+        movq xmm4, [r11 - 1]  ; xmm4 = ←
+        movq xmm6, [r11 + 1]  ; xmm6 = →
+        movq xmm7, [r12 - 1]  ; xmm7 = ↙
+        movq xmm8, [r12]      ; xmm8 = ↓
+        movq xmm9, [r12 + 1]  ; xmm9 = ↘
         movdqu xmm10, xmm3      ; xmm10 = ↗
         movdqu xmm11, xmm9      ; xmm11 = ↘
         ; Calculamos TotalGx.
-        psubw xmm3, xmm1        ; xmm3[31:0] = ____ | ____ | ↗₁ - ↖₁ | ↗₀ - ↖₀
-        psubw xmm9, xmm7        ; xmm9[31:0] = ____ | ____ | ↘₁ - ↙₁ | ↘₀ - ↙₀
-        psubw xmm6, xmm4        ; xmm6[31:0] = ____ | ____ | →₁ - ←₁ | →₀ - ←₀
+        psubb xmm3, xmm1        ; xmm3[31:0] = ____ | ____ | ↗₁ - ↖₁ | ↗₀ - ↖₀
+        psubb xmm9, xmm7        ; xmm9[31:0] = ____ | ____ | ↘₁ - ↙₁ | ↘₀ - ↙₀
+        psubb xmm6, xmm4        ; xmm6[31:0] = ____ | ____ | →₁ - ←₁ | →₀ - ←₀
 
-        punpcklbw xmm6, xmm15   ; xmm6[31:0] = 0x00 | →₁ - ←₁ | 0x00 | →₀ - ←₀
-        psllw xmm6, 1           ; xmm6[31:0] = 2 * (→₁ - ←₁) | 2 * (→₀ - ←₀)
-        punpcklbw xmm3, xmm15   ; xmm3[31:0] = 0x00 | ↗₁ - ↖₁ | 0x00 | ↗₀ - ↖₀
-        punpcklbw xmm9, xmm15   ; xmm9[31:0] = 0x00 | ↘₁ - ↙₁ | 0x00 | ↘₀ - ↙₀    
+        pxor xmm14, xmm14
+        pcmpgtb xmm14, xmm6
+        punpcklbw xmm6, xmm14   ; xmm6[31:0] = 0xFF | →₁ - ←₁ | 0x00 | →₀ - ←₀
+        psllw xmm6, 1           ; xmm6[31:0] = 2 * (→₁ - ←₁)  | 2 * (→₀ - ←₀)
+        
+        pxor xmm14, xmm14
+        pcmpgtb xmm14, xmm3
+        punpcklbw xmm3, xmm14   ; xmm3[31:0] = 0x00 | ↗₁ - ↖₁ | 0x00 | ↗₀ - ↖₀
+
+        pxor xmm14, xmm14
+        pcmpgtb xmm14, xmm9
+        punpcklbw xmm9, xmm14   ; xmm9[31:0] = 0x00 | ↘₁ - ↙₁ | 0x00 | ↘₀ - ↙₀    
 
         paddw xmm3, xmm9        ; xmm3[15:0] = (↗₀ - ↖₀) + (↘₀ - ↙₀)
         paddw xmm3, xmm6        ; xmm3[15:0] = (↗₀ - ↖₀) + (↘₀ - ↙₀) + 2*(→₀ - ←₀)
+        ;pabsw xmm3, xmm3
         movdqu xmm0, xmm3
+
         pcmpgtw xmm15, xmm3     ; xmm15[15:0]= xmm3[15:0] < 0? 1 : 0
         ; Tenemos en xmm15 la mascara de negativos
         ; Haciendo un and con xmm3 nos quedamos solamente los negativos.
@@ -90,17 +101,27 @@ Bordes_asm:
 ; Gy
         pxor xmm15, xmm15
         
-        psubw xmm7, xmm1        ; xmm7[31:0] = ____ | ____ | ↙₁ - ↖₁ | ↙₀ - ↖₀
-        psubw xmm11, xmm10      ; xmm11[31:0]= ____ | ____ | ↘₁ - ↗₁ | ↘₀ - ↗₀
-        psubw xmm8, xmm2        ; xmm8[31:0] = ____ | ____ | ↓₁ - ↑₁ | ↓₀ - ↑₀
-
+        psubb xmm7, xmm1        ; xmm7[31:0] = ____ | ____ | ↙₁ - ↖₁ | ↙₀ - ↖₀
+        psubb xmm11, xmm10      ; xmm11[31:0]= ____ | ____ | ↘₁ - ↗₁ | ↘₀ - ↗₀
+        psubb xmm8, xmm2        ; xmm8[31:0] = ____ | ____ | ↓₁ - ↑₁ | ↓₀ - ↑₀
+        
+        pxor xmm14, xmm14
+        pcmpgtb xmm14, xmm8
         punpcklbw xmm8, xmm15   ; xmm8[31:0] = 0x00 | ↓₁ - ↑₁ | 0x00 | ↓₀ - ↑₀
-        psllw xmm8, 1           ; xmm8[31:0] = 2 * (↓₁ - ↑₁) | 2 * (↓₀ - ↑₀)
-        punpcklbw xmm7, xmm15   ; xmm7[31:0] = 0x00 | ↙₁ - ↖₁ | 0x00 | ↙₀ - ↖₀
-        punpcklbw xmm11, xmm15  ; xmm11[31:0]= 0x00 | ↘₁ - ↗₁ | 0x00 | ↘₀ - ↗₀
+        psllw xmm8, 1           ; xmm8[31:0] = 2 * (↓₁ - ↑₁)  | 2 * (↓₀ - ↑₀)
+        
+        pxor xmm14, xmm14
+        pcmpgtb xmm14, xmm7
+        punpcklbw xmm7, xmm14   ; xmm7[31:0] = 0x00 | ↙₁ - ↖₁ | 0x00 | ↙₀ - ↖₀
+        
+        pxor xmm14, xmm14
+        pcmpgtb xmm14, xmm11
+        punpcklbw xmm11, xmm14  ; xmm11[31:0]= 0x00 | ↘₁ - ↗₁ | 0x00 | ↘₀ - ↗₀
 
         paddw xmm7, xmm11
         paddw xmm7, xmm8
+        ;pabsw xmm7, xmm7
+
         movdqu xmm12, xmm7
         ; Generamos la mascara de los negativos para poder hacerles el
         ; valor absoluto.       ; xmm7  =   >0   |   >0   | .... |   <0  
@@ -113,9 +134,10 @@ Bordes_asm:
         paddw xmm7, xmm14       ; xmm7  =   >0   |   >0   | .... |   >0  
         pandn xmm15, xmm12
         por xmm7, xmm15
+
         paddw xmm0, xmm7
-        packsswb xmm0, xmm0
-        movdqu [r13], xmm0
+        packuswb xmm0, xmm0
+        movd [r13], xmm0 ;!!
 
         add r10, 8
         add r11, 8
