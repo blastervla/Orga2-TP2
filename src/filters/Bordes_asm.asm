fa06ef79c10abc3b1b2e996acdd65e029669d736
arehor-ignacio <arehor.ignacio@gmail.com>
2019-09-29 20:19:04 -0300

    pasan tests memoria y diferencias

--- src/filters/Bordes_asm.asm
+++ src/filters/Bordes_asm.asm
@@ -1,4 +1,5 @@
 section .rodata
+    edges: TIMES 8 db 255
 section .text
 
 global Bordes_asm
@@ -15,137 +16,206 @@ Bordes_asm:
     push r13
     push r14
 
-    ;mov r10, rdi
-    ;mov r11, rsi    
-
-    mov eax, ecx        ; eax = height
-    mov eax, eax        ; rax = 0x00000000 | height
+    mov ecx, ecx
+    mov rax, rcx        ; rax = 0x00000000 | height
     mov edx, edx        ; rdx = 0x00000000 | width
     mov r8d, r8d        ; r8  = 0x00000000 | src_row_size
 ;   Le decrementamos uno porque queremos recorrer hasta la anteúltima fila.
 ;   Seguimos teneiendo que rax = 8*k pues width es multiplo de 8
-    dec rax             ; rax = 0x00000000 | height - 1
+    dec rax             ; rax = width - 1
     mul rdx             ; rax = width * (height - 1) = width * height - width
-    sub rax, r8
-    ;sub rax, 1
-    
-    lea r11, [rdi + r8 +1]
-    lea r10, [r11]
-    sub r10, r8
-    lea r12, [r11 +r8]
-    lea r13, [rsi + r8 +1]
-    lea r14, [rdi + rax]        ; Define pixel_size
-    
+
+    lea r10, [rdi]              ; r10 --> (-1,-1) | (0,-1) | (1,-1)
+    lea r11, [rdi + r8]         ; r11 --> (-1, 0) | (0, 0) | (1, 0)
+    lea r12, [rdi + r8*2]       ; r12 --> (-1, 1) | (0, 1) | (1, 1)
+    lea r13, [rsi + r8]         ; Donde (0, 0) es el pixel que estamos
+    lea r14, [rdi + rax - 8]    ; procesando.
+                                ; Vamos a hacer la última iteración aparte pues
+    pxor xmm15, xmm15           ; si no lo hacemos, estamos haciendo un invalid
+                                ; read cuando accedemos a r12.
+
+    cmp rcx, 2      ; Caso borde donde solo hay menos de 3 filas. Las posiciónes
+    jle .edges   ; apuntadas por r12 y r11 están fuera de rango.
     .loop:
         cmp r11, r14
-        jge .end
+        je .end_loop
 
+        movq xmm1, [r10]        ; xmm1 : ↖ - Esquina superior izquierda.
+        movq xmm2, [r10 + 1]    ; xmm2 : ^ - Medio superior.
+        movq xmm3, [r10 + 2]    ; xmm3 : ↗ - Esquina superior derecha.
+        movq xmm4, [r11]        ; xmm4 : < - Medio lateral izquierdo.
+        movq xmm5, [r11 + 2]    ; xmm5 : > - Medio lateral derecho.
+        movq xmm6, [r12]        ; xmm6 : ↙ - Esquina inferior izquierda.
+        movq xmm7, [r12 + 1]    ; xmm7 : v - Medio inferior.
+        movq xmm8, [r12 + 2]    ; xmm8 : ↘ - Esquina inferior derecha.
+
+        movdqu xmm9, xmm3       ; Copiamos xmm3, xmm8 pues los tenemos
+        movdqu xmm10, xmm8      ; que usar para el calculo de Total_Gy.
+
+        ;pxor xmm15, xmm15
+
+        punpcklbw xmm1, xmm15   ; xmm1[31:0] = 0x00 | ↖₁ | 0x00 | ↖₀
+        punpcklbw xmm3, xmm15   ; xmm3[31:0] = 0x00 | ↗₁ | 0x00 | ↗₀
+        psubw xmm3, xmm1        ; xmm3[31:0] = ↗₁ - ↖₁ | ↗₀ - ↖₀
+
+        punpcklbw xmm6, xmm15   ; xmm6[31:0] = 0x00 | ↙₁ | 0x00 | ↙₀
+        punpcklbw xmm8, xmm15   ; xmm8[31:0] = 0x00 | ↘₁ | 0x00 | ↘₀
+        psubw xmm8, xmm6        ; xmm8[31:0] = ↘₁ - ↙₁ | ↘₀ - ↙₀
+
+        punpcklbw xmm4, xmm15   ; xmm4[31:0] = 0x00 | <--₁ | 0x00 | <--₀
+        punpcklbw xmm5, xmm15   ; xmm5[31:0] = 0x00 | -->₁ | 0x00 | -->₀
+        psubw xmm5, xmm4        ; xmm5[31:0] = -->₁ - <--₁ | -->₀ - <--₀
+        psllw xmm5, 1           ; xmm5[31:0] = 2*(-->₁ - <--₁) | 2*(-->₀ - <--₀)
+
+        paddw xmm3, xmm8
+        paddw xmm3, xmm5
         
-        pxor xmm15, xmm15
+        ;Valor Absoluto
+        ; movdqu xmm14, xmm3
+        ; pcmpgtw xmm15, xmm3
+        ; movdqu xmm13, xmm15
+        ; psrlw xmm13, 15
 
-;       ↖ ↑ ↗  =  xmm1 xmm2 xmm3
-;       ← ⋅ →  =  xmm4   0   xmm6
-;       ↙ ↓ ↘  =  xmm7 xmm8 xmm9
-        movq xmm1, [r10 - 1]  ; xmm1 = ↖
-        movq xmm2, [r10]      ; xmm2 = ↑
-        movq xmm3, [r10 + 1]  ; xmm3 = ↗
-        movq xmm4, [r11 - 1]  ; xmm4 = ←
-        movq xmm6, [r11 + 1]  ; xmm6 = →
-        movq xmm7, [r12 - 1]  ; xmm7 = ↙
-        movq xmm8, [r12]      ; xmm8 = ↓
-        movq xmm9, [r12 + 1]  ; xmm9 = ↘
-        movdqu xmm10, xmm3      ; xmm10 = ↗
-        movdqu xmm11, xmm9      ; xmm11 = ↘
-        ; Calculamos TotalGx.
-        psubb xmm3, xmm1        ; xmm3[31:0] = ____ | ____ | ↗₁ - ↖₁ | ↗₀ - ↖₀
-        psubb xmm9, xmm7        ; xmm9[31:0] = ____ | ____ | ↘₁ - ↙₁ | ↘₀ - ↙₀
-        psubb xmm6, xmm4        ; xmm6[31:0] = ____ | ____ | →₁ - ←₁ | →₀ - ←₀
+        ; pand xmm3, xmm15
+        ; pxor xmm3, xmm15
+        ; paddw xmm3, xmm13
 
-        pxor xmm14, xmm14
-        pcmpgtb xmm14, xmm6
-        punpcklbw xmm6, xmm14   ; xmm6[31:0] = 0xFF | →₁ - ←₁ | 0x00 | →₀ - ←₀
-        psllw xmm6, 1           ; xmm6[31:0] = 2 * (→₁ - ←₁)  | 2 * (→₀ - ←₀)
+        ; pandn xmm15, xmm14
+        ; por xmm3, xmm15
+
+        pabsw xmm3, xmm3
+        movdqu xmm0, xmm3      
+
+;       Ya hicimos estos unpacks cuando calculamos Total_Gx
+        ;punpcklbw xmm1, xmm15  ; xmm1[31:0] = 0x00 | ↖₁ | 0x00 | ↖₀
+        ;punpcklbw xmm6, xmm15  ; xmm6[31:0] = 0x00 | ↙₁ | 0x00 | ↙₀
+        psubw xmm6, xmm1        ; xmm6[31:0] = ↙₁ - ↖₁ | ↙₀ - ↖₀
+
+        punpcklbw xmm9, xmm15   ; xmm3[31:0]  = 0x00 | ↗₁ | 0x00 | ↗₀
+        punpcklbw xmm10, xmm15  ; xmm10[31:0] = 0x00 | ↘₁ | 0x00 | ↘₀
+        psubw xmm10, xmm9       ; xmm10[31:0] = ↘₁ - ↗₁ | ↘₀ - ↗₀
+
+        punpcklbw xmm2, xmm15   ; xmm2[31:0] = 0x00 | ^₁ | 0x00 | ^₀
+        punpcklbw xmm7, xmm15   ; xmm7[31:0] = 0x00 | v₁ | 0x00 | v₀
+        psubw xmm7, xmm2        ; xmm7[31:0] = v₁ - ^₁ | v₀ - ^₀
+        psllw xmm7, 1           ; xmm7[31:0] = 2*(v₁ - ^₁) | 2*(v₀ - ^₀)
+
+        paddw xmm6, xmm10
+        paddw xmm6, xmm7
+
+        pabsw xmm6, xmm6
         
-        pxor xmm14, xmm14
-        pcmpgtb xmm14, xmm3
-        punpcklbw xmm3, xmm14   ; xmm3[31:0] = 0x00 | ↗₁ - ↖₁ | 0x00 | ↗₀ - ↖₀
-
-        pxor xmm14, xmm14
-        pcmpgtb xmm14, xmm9
-        punpcklbw xmm9, xmm14   ; xmm9[31:0] = 0x00 | ↘₁ - ↙₁ | 0x00 | ↘₀ - ↙₀    
-
-        paddw xmm3, xmm9        ; xmm3[15:0] = (↗₀ - ↖₀) + (↘₀ - ↙₀)
-        paddw xmm3, xmm6        ; xmm3[15:0] = (↗₀ - ↖₀) + (↘₀ - ↙₀) + 2*(→₀ - ←₀)
-        ;pabsw xmm3, xmm3
-        movdqu xmm0, xmm3
-
-        pcmpgtw xmm15, xmm3     ; xmm15[15:0]= xmm3[15:0] < 0? 1 : 0
-        ; Tenemos en xmm15 la mascara de negativos
-        ; Haciendo un and con xmm3 nos quedamos solamente los negativos.
-        pand xmm3, xmm15        ; xmm3[15:0] = xmm3[15:0] < 0? xmm3[15:0] : 0
-        ; Haciendo un xor invertimos los bits de los negativos.
-        ; Los que no eran negativos, se quedan en 0.
-        pxor xmm3, xmm15        ; xmm3[15:0] = xmm3[15:0] < 0? !xmm3[15:0] : 0
-        movdqu xmm14, xmm15
-        psrlw xmm14, 15         ; Ej: xmm14 = 1 | 0 | 1 | 1
-        ; Sumando obtenemos el inverso aditivio de todos los negativos.
-        paddw xmm3, xmm14       ; xmm3[15:0] = xmm3[15:0] < 0? -xmm3[15:0] : 0
-        ;movdqu xmm14, xmm15
-        ; Si hacemos un and negado entre la mascara de negativos y xmm0
-        ; Obtenemos en xmm15, los positivos y 0 donde había negativos.
-        pandn xmm15, xmm0
-        ; Si hacemos un or contra el registro con los inversos aditivos,
-        ; tenemos el resultado.
-        por xmm3, xmm15         ; xmm3[15:0] = abs(xmm3[15:0])
-        movdqu xmm0, xmm3
-; Gy
-        pxor xmm15, xmm15
-        
-        psubb xmm7, xmm1        ; xmm7[31:0] = ____ | ____ | ↙₁ - ↖₁ | ↙₀ - ↖₀
-        psubb xmm11, xmm10      ; xmm11[31:0]= ____ | ____ | ↘₁ - ↗₁ | ↘₀ - ↗₀
-        psubb xmm8, xmm2        ; xmm8[31:0] = ____ | ____ | ↓₁ - ↑₁ | ↓₀ - ↑₀
-        
-        pxor xmm14, xmm14
-        pcmpgtb xmm14, xmm8
-        punpcklbw xmm8, xmm15   ; xmm8[31:0] = 0x00 | ↓₁ - ↑₁ | 0x00 | ↓₀ - ↑₀
-        psllw xmm8, 1           ; xmm8[31:0] = 2 * (↓₁ - ↑₁)  | 2 * (↓₀ - ↑₀)
-        
-        pxor xmm14, xmm14
-        pcmpgtb xmm14, xmm7
-        punpcklbw xmm7, xmm14   ; xmm7[31:0] = 0x00 | ↙₁ - ↖₁ | 0x00 | ↙₀ - ↖₀
-        
-        pxor xmm14, xmm14
-        pcmpgtb xmm14, xmm11
-        punpcklbw xmm11, xmm14  ; xmm11[31:0]= 0x00 | ↘₁ - ↗₁ | 0x00 | ↘₀ - ↗₀
-
-        paddw xmm7, xmm11
-        paddw xmm7, xmm8
-        ;pabsw xmm7, xmm7
-
-        movdqu xmm12, xmm7
-        ; Generamos la mascara de los negativos para poder hacerles el
-        ; valor absoluto.       ; xmm7  =   >0   |   >0   | .... |   <0  
-        pcmpgtw xmm15, xmm7     ; xmm15 = 0x0000 | 0x0000 | .... | 0xFFFF 
-        pand xmm7, xmm15
-        pxor xmm7, xmm15
-        ; Tenemos en xmm7 los negativos negados bit a bit. Hay que sumarles 1.
-        movdqu xmm14, xmm15     
-        psrlw xmm14, 15         ; xmm14 = 0x0000 | 0x0000 | .... | 0x0001
-        paddw xmm7, xmm14       ; xmm7  =   >0   |   >0   | .... |   >0  
-        pandn xmm15, xmm12
-        por xmm7, xmm15
-
-        paddw xmm0, xmm7
+        paddw xmm0, xmm6
         packuswb xmm0, xmm0
-        movd [r13], xmm0 ;!!
+
+        movq [r13 + 1], xmm0
+
+        add r13, 8
+        add r12, 8
+        add r11, 8
+        add r10, 8
+        jmp .loop
+
+    .end_loop:
+        movq xmm1, [r10]        ; Ultimos 8 pixeles a procear.
+        movq xmm4, [r11]        ; r10 -> fila n-2 : ____ | ____ | ... | ____
+        movq xmm6, [r12]        ; r11 -> fila n-1 : ____ | ____ | ... | ____
+                                ; r12 -> fila n   : ____ | ____ | ... | ____
+        movdqu xmm2, xmm1       ; xmm2[63:0] = p_7 | p_6 | p_5 | p_4 | ... | p_0
+        psrldq xmm2, 1          ; xmm2[63:0] = ___ | p_7 | p_6 | p_5 | ... | p_1
+        movdqu xmm3, xmm1       
+        psrldq xmm3, 2           ; xmm3[63:0] = ___ | ___ | p_7 | p_6 | ... | p_2
+
+        movdqu xmm5, xmm4       ; xmm5[63:0] = q_7 | q_6 | q_5 | q_4 | ... | q_0
+        psrldq xmm5, 2          ; xmm5[63:0] = ___ | ___ | q_7 | q_6 | ... | q_2
+
+        movdqu xmm7, xmm6       ; xmm7[63:0] = r_7 | r_6 | r_5 | r_4 | ... | r_0
+        psrldq xmm7, 1          ; xmm7[63:0] = ___ | r_7 | r_6 | r_5 | ... | r_1
+        movdqu xmm8, xmm6
+        psrldq xmm8, 2          ; xmm8[63:0] = ___ | ___ | r_7 | r_6 | ... | r_2
+;       Ahora operamos de la misma manera que lo hicimos arriba. Notemos que,
+;       aunque hicimos el shift necesario para que los pixeles vecinos nos
+;       queden alineados, esto va a hacer que escribamos "basura" en la
+;       fila próxima. Esto no es un problema ya es la última fila y la debemos
+;       rellenar al final del procesar la imagen.
+        movdqu xmm9, xmm3
+        movdqu xmm10, xmm8
+
+        punpcklbw xmm1, xmm15   ; xmm1[31:0] = 0x00 | ↖₁ | 0x00 | ↖₀
+        punpcklbw xmm3, xmm15   ; xmm3[31:0] = 0x00 | ↗₁ | 0x00 | ↗₀
+        psubw xmm3, xmm1        ; xmm3[31:0] = ↗₁ - ↖₁ | ↗₀ - ↖₀
+
+        punpcklbw xmm6, xmm15   ; xmm6[31:0] = 0x00 | ↙₁ | 0x00 | ↙₀
+        punpcklbw xmm8, xmm15   ; xmm8[31:0] = 0x00 | ↘₁ | 0x00 | ↘₀
+        psubw xmm8, xmm6        ; xmm8[31:0] = ↘₁ - ↙₁ | ↘₀ - ↙₀
+
+        punpcklbw xmm4, xmm15   ; xmm4[31:0] = 0x00 | <--₁ | 0x00 | <--₀
+        punpcklbw xmm5, xmm15   ; xmm5[31:0] = 0x00 | -->₁ | 0x00 | -->₀
+        psubw xmm5, xmm4        ; xmm5[31:0] = -->₁ - <--₁ | -->₀ - <--₀
+        psllw xmm5, 1           ; xmm5[31:0] = 2*(-->₁ - <--₁) | 2*(-->₀ - <--₀)
+
+        paddw xmm3, xmm8
+        paddw xmm3, xmm5
+
+        pabsw xmm3, xmm3
+        movdqu xmm0, xmm3
+
+        psubw xmm6, xmm1        ; xmm6[31:0] = ↙₁ - ↖₁ | ↙₀ - ↖₀
+
+        punpcklbw xmm9, xmm15   ; xmm3[31:0]  = 0x00 | ↗₁ | 0x00 | ↗₀
+        punpcklbw xmm10, xmm15  ; xmm10[31:0] = 0x00 | ↘₁ | 0x00 | ↘₀
+        psubw xmm10, xmm9       ; xmm10[31:0] = ↘₁ - ↗₁ | ↘₀ - ↗₀
+
+        punpcklbw xmm2, xmm15   ; xmm2[31:0] = 0x00 | ^₁ | 0x00 | ^₀
+        punpcklbw xmm7, xmm15   ; xmm7[31:0] = 0x00 | v₁ | 0x00 | v₀
+        psubw xmm7, xmm2        ; xmm7[31:0] = v₁ - ^₁ | v₀ - ^₀
+        psllw xmm7, 1           ; xmm7[31:0] = 2*(v₁ - ^₁) | 2*(v₀ - ^₀)
+
+        paddw xmm6, xmm10
+        paddw xmm6, xmm7
+
+        pabsw xmm6, xmm6
+        paddw xmm0, xmm6
+        packuswb xmm0, xmm0
+
+        movq [r13 + 1], xmm0
+
+; -------------- BORDES ------------
+    .edges:
+        xor r10, r10                        
+        movq xmm0, [edges]
+
+    .horizontal:
+        cmp r10, r8
+        je .end_horizontal
+        
+        lea r11, [r10 + rax]
+        
+        movq [rsi + r10], xmm0
+        movq [rsi + r11], xmm0
 
         add r10, 8
-        add r11, 8
-        add r12, 8
-        add r13, 8
-        jmp .loop
-   .end:
+        jmp .horizontal
+
+    .end_horizontal:
+        lea r12, [rsi + rax - 1]
+        lea rsi, [rsi + r8 - 1]
+        mov r13w, [edges]
+
+    .vertical:
+        cmp rsi, r12
+        je .end
+
+        mov [rsi], r13w
+
+        lea rsi, [rsi + r8]
+        jmp .vertical
+
+    .end:
+        mov [rsi], r13w
+
         pop r14
         pop r13
         pop r12
+
         ret
--- src/filters/comentarios.asm
+++ src/filters/comentarios.asm
@@ -152,3 +152,152 @@
 
 ;     pop rbp
 ; ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cmp r11, r14
+        jge .end
+
+        
+        pxor xmm15, xmm15
+
+;       ↖ ↑ ↗  =  xmm1 xmm2 xmm3
+;       ← ⋅ →  =  xmm4   0   xmm6
+;       ↙ ↓ ↘  =  xmm7 xmm8 xmm9
+        movq xmm1, [r10 - 1]  
+        movq xmm2, [r10]      
+        movq xmm3, [r10 + 1]  
+        movq xmm4, [r11 - 1]  
+        movq xmm6, [r11 + 1]  
+        movq xmm7, [r12 - 1]  
+        movq xmm8, [r12]      
+        movq xmm9, [r12 + 1]  
+       
+        ; Calculamos TotalGx.
+        
+        punpcklbw xmm1, xmm15
+        punpcklbw xmm2, xmm15
+        punpcklbw xmm3, xmm15
+        punpcklbw xmm4, xmm15
+        punpcklbw xmm6, xmm15
+        punpcklbw xmm7, xmm15
+        punpcklbw xmm8, xmm15
+        punpcklbw xmm9, xmm15
+
+        movdqu xmm10, xmm3      ; xmm10 = ↗
+        movdqu xmm11, xmm9      ; xmm11 = ↘
+
+        ; psubw xmm3, xmm1        ; xmm3[31:0] = ____ | ____ | ↗₁ - ↖₁ | ↗₀ - ↖₀
+        ; psubw xmm9, xmm7        ; xmm9[31:0] = ____ | ____ | ↘₁ - ↙₁ | ↘₀ - ↙₀
+        ; psubw xmm6, xmm4        ; xmm6[31:0] = ____ | ____ | →₁ - ←₁ | →₀ - ←₀
+
+        ; ;pxor xmm14, xmm14
+        ; ;pcmpgtb xmm14, xmm6
+        ; ;punpcklbw xmm6, xmm14   ; xmm6[31:0] = 0xFF | →₁ - ←₁ | 0x00 | →₀ - ←₀
+        ; psllw xmm6, 1           ; xmm6[31:0] = 2 * (→₁ - ←₁)  | 2 * (→₀ - ←₀)
+        
+        ; ;pxor xmm14, xmm14
+        ; ;pcmpgtb xmm14, xmm3
+        ; ;punpcklbw xmm3, xmm14   ; xmm3[31:0] = 0x00 | ↗₁ - ↖₁ | 0x00 | ↗₀ - ↖₀
+
+        ; ;pxor xmm14, xmm14
+        ; ;pcmpgtb xmm14, xmm9
+        ; ;punpcklbw xmm9, xmm14   ; xmm9[31:0] = 0x00 | ↘₁ - ↙₁ | 0x00 | ↘₀ - ↙₀    
+
+        ; paddw xmm3, xmm9        ; xmm3[15:0] = (↗₀ - ↖₀) + (↘₀ - ↙₀)
+        ; paddw xmm3, xmm6        ; xmm3[15:0] = (↗₀ - ↖₀) + (↘₀ - ↙₀) + 2*(→₀ - ←₀)
+        ; ;pabsw xmm3, xmm3
+        ; movdqu xmm0, xmm3
+
+        ; pcmpgtw xmm15, xmm3     ; xmm15[15:0]= xmm3[15:0] < 0? 1 : 0
+        ; ; Tenemos en xmm15 la mascara de negativos
+        ; ; Haciendo un and con xmm3 nos quedamos solamente los negativos.
+        ; pand xmm3, xmm15        ; xmm3[15:0] = xmm3[15:0] < 0? xmm3[15:0] : 0
+        ; ; Haciendo un xor invertimos los bits de los negativos.
+        ; ; Los que no eran negativos, se quedan en 0.
+        ; pxor xmm3, xmm15        ; xmm3[15:0] = xmm3[15:0] < 0? !xmm3[15:0] : 0
+        ; movdqu xmm14, xmm15
+        ; psrlw xmm14, 15         ; Ej: xmm14 = 1 | 0 | 1 | 1
+        ; ; Sumando obtenemos el inverso aditivio de todos los negativos.
+        ; paddw xmm3, xmm14       ; xmm3[15:0] = xmm3[15:0] < 0? -xmm3[15:0] : 0
+        ; ;movdqu xmm14, xmm15
+        ; ; Si hacemos un and negado entre la mascara de negativos y xmm0
+        ; ; Obtenemos en xmm15, los positivos y 0 donde había negativos.
+        ; pandn xmm15, xmm0
+        ; ; Si hacemos un or contra el registro con los inversos aditivos,
+        ; ; tenemos el resultado.
+        ; por xmm3, xmm15         ; xmm3[15:0] = abs(xmm3[15:0])
+        ; movdqu xmm0, xmm3
+; pxor xmm15, xmm15
+        
+        psubw xmm7, xmm1        ; xmm7[31:0] = ____ | ____ | ↙₁ - ↖₁ | ↙₀ - ↖₀
+        psubw xmm11, xmm10      ; xmm11[31:0]= ____ | ____ | ↘₁ - ↗₁ | ↘₀ - ↗₀
+        psubw xmm8, xmm2        ; xmm8[31:0] = ____ | ____ | ↓₁ - ↑₁ | ↓₀ - ↑₀
+        
+        pxor xmm14, xmm14
+        ;pcmpgtb xmm14, xmm8
+        ;punpcklbw xmm8, xmm15   ; xmm8[31:0] = 0x00 | ↓₁ - ↑₁ | 0x00 | ↓₀ - ↑₀
+        psllw xmm8, 1           ; xmm8[31:0] = 2 * (↓₁ - ↑₁)  | 2 * (↓₀ - ↑₀)
+        
+        ;pxor xmm14, xmm14
+        ;pcmpgtb xmm14, xmm7
+        ;punpcklbw xmm7, xmm14   ; xmm7[31:0] = 0x00 | ↙₁ - ↖₁ | 0x00 | ↙₀ - ↖₀
+        
+        ;pxor xmm14, xmm14
+        ;pcmpgtb xmm14, xmm11
+        ;punpcklbw xmm11, xmm14  ; xmm11[31:0]= 0x00 | ↘₁ - ↗₁ | 0x00 | ↘₀ - ↗₀
+
+        paddw xmm7, xmm11
+        paddw xmm7, xmm8
+        ;pabsw xmm7, xmm7
+
+        movdqu xmm12, xmm7
+        ; Generamos la mascara de los negativos para poder hacerles el
+        ; valor absoluto.       ; xmm7  =   >0   |   >0   | .... |   <0  
+        pcmpgtw xmm15, xmm7     ; xmm15 = 0x0000 | 0x0000 | .... | 0xFFFF 
+        pand xmm7, xmm15
+        pxor xmm7, xmm15
+        ; Tenemos en xmm7 los negativos negados bit a bit. Hay que sumarles 1.
+        movdqu xmm14, xmm15     
+        psrlw xmm14, 15         ; xmm14 = 0x0000 | 0x0000 | .... | 0x0001
+        paddw xmm7, xmm14       ; xmm7  =   >0   |   >0   | .... |   >0  
+        pandn xmm15, xmm12
+        por xmm7, xmm15
+
+        ;paddw xmm0, xmm7
+;        packuswb xmm0, xmm0
+        packuswb xmm7, xmm7
+
+        movd [r13], xmm7 ;!!
+        ; Gy
+        add r10, 8
+        add r11, 8
+        add r12, 8
+        add r13, 8
+        jmp .loop
+   .end:
+        pop r14
+        pop r13
+        pop r12
+        ret
